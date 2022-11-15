ee18041d83531e697b3d564569183a0bccf361dc
Georg Veichtlbauer <georg@vware.at>
2022-11-15 11:23:39 +0000

    sdm845-common: Fix PowerOffAlarm * The APK is prepared to not run as system user * Blobs grabbed from: lge/caymanslm/caymanslm:12/SKQ1.211103.001/2222823249237:user/release-keys
    Change-Id: I3496232d468c671f7ef67b7bb6050978388d6ccf

--- sdm845-common/sdm845-common-vendor.mk
+++ sdm845-common/sdm845-common-vendor.mk
@@ -802,9 +802,9 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    PowerOffAlarm \
     HotwordEnrollmentOKGoogleWCD9340 \
     HotwordEnrollmentXGoogleWCD9340 \
+    PowerOffAlarm \
     QtiTelephonyService \
     uceShimService \
     dpmserviceapp \
